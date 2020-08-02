d96b68342f69aa08a70fb302932caa65427598c1
Norm Brandinger <n.brandinger@gmail.com>
2020-08-02 12:52:20 -0400

    Update mod_mosquitto.c
    Revert bad commit.

--- mod_mosquitto.c
+++ mod_mosquitto.c
@@ -123,8 +123,7 @@ SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_mosquitto_shutdown)
 	remove_cli_api();
 
 	mosq_shutdown();
-
-	switch_core_hash_destroy(&mosquitto_globals.profiles);
+	
 	switch_mutex_destroy(mosquitto_globals.profiles_mutex);
 	switch_mutex_unlock(mosquitto_globals.mutex);
 	switch_mutex_destroy(mosquitto_globals.mutex);
